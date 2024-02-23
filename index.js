b9c4a13337b67d530c7c8013d8d87492974dbfa0
unknown <maryamihsan601@gmail.com>
2024-02-23 16:23:46 +0300

    array method and foreach loop

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-function processAges(ages) {
   //!your solution should be inside the function.
   //1-Use a for loop to iterate over the ages array and double each age. Store the doubled ages in a new array called doubledAges and Print the doubledAges array to the console.
 
@@ -9,8 +8,28 @@ function processAges(ages) {
   //4-Use the map method to create a new array called ageStrings that contains string representations of each age in the ages array.
 
   //5-please make sure to name your arrays(variables) accordingly to the names in the return statement.
-
+  let ages=[25, 18, 12, 16, 40]
+  let doubledAges = []
+  let adults = []
+function processAges(ages) {
+  for( i = 0 ; i < ages.length ; i++){
+    doubledAges[i] = ages[i]*2 
+  }
+  ages.forEach(function (age , index) {
+    console.log(age)
+  });
+  adults = ages.filter(function(age){
+    return age >= 18;
+  }); 
+  ageStrings = ages.map(function(age){
+    return age.toString()})
   return { doubledAges, adults, ageStrings };
 }
-
+processAges(ages)
 module.exports = processAges;
+
+
+
+
+
+ 
